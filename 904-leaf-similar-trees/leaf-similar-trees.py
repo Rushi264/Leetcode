a1caec6e458d0e8ae791b9615f43bf6f7ffb7127
Rushikesh Sanjay Deshmukh <138613542+Rushi264@users.noreply.github.com>
2024-01-10 05:06:26 -0800

    Time: 40 ms (53.31%) | Memory: 17.3 MB (30.86%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.py
+++ 904-leaf-similar-trees/leaf-similar-trees.py
@@ -1,27 +1,17 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        
-        def leaf_checker(root, leaf):
+        def collect_leaf_values(root, leaf_values):
             if not root:
-                return 
-            if not root.left and not root.right:
-                leaf.append(root.val)
                 return
-            leaf_checker(root.left, leaf)
-            leaf_checker(root.right, leaf)
+            if not root.left and not root.right:
+                leaf_values.append(root.val)
+            collect_leaf_values(root.left, leaf_values)
+            collect_leaf_values(root.right, leaf_values)
 
-    
-        
-        leaf1 = []
-        leaf2 = []
-        leaf_checker(root1, leaf1)
-        leaf_checker(root2, leaf2)
+        leaf_values1 = []
+        leaf_values2 = []
 
-        return leaf1 == leaf2
+        collect_leaf_values(root1, leaf_values1)
+        collect_leaf_values(root2, leaf_values2)
 
+        return leaf_values1 == leaf_values2

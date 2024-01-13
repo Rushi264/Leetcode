e3a1f3022f059789cc2a7cdc3605bc772fe718bb
Rushikesh Sanjay Deshmukh <138613542+Rushi264@users.noreply.github.com>
2024-01-12 18:21:35 -0800

    Time: 154 ms (63.26%) | Memory: 18.1 MB (5.24%) - LeetSync

---
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        count = 0
+        hashmap1, hashmap2 = {}, {}
+
+        # Counting characters in s
+        for char in s:
+            if char in hashmap1:
+                hashmap1[char] += 1
+            else:
+                hashmap1[char] = 1
+
+        # Counting characters in t
+        for char in t:
+            if char in hashmap2:
+                hashmap2[char] += 1
+            else:
+                hashmap2[char] = 1
+
+        # Calculating the minimum number of steps
+        for char in hashmap1:
+            count += max(0, hashmap1[char] - hashmap2.get(char, 0))
+
+        return count

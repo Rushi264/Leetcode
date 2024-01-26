f3733a1da5a12b90952649c5b8d3bcb021e4c632
Rushikesh Sanjay Deshmukh <138613542+Rushi264@users.noreply.github.com>
2024-01-25 22:45:54 -0800

    Time: 65 ms (97.69%) | Memory: 17.8 MB (84.39%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.py
+++ 576-out-of-boundary-paths/out-of-boundary-paths.py
@@ -1,29 +1,25 @@
 class Solution:
-    def findPaths(self, m: int, n: int, N: int, x: int, y: int) -> int:
-        M = 1000000000 + 7
-        dp = [[0] * n for _ in range(m)]
-        dp[x][y] = 1
-        count = 0
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        dp = [[[None] * (maxMove + 1) for _ in range(n)] for _ in range(m)]
+        self.m = m
+        self.n = n
+        self.mod = 10**9 + 7
+        return self.helper(maxMove, startRow, startColumn, dp)
 
-        for moves in range(1, N + 1):
-            temp = [[0] * n for _ in range(m)]
+    def helper(self, maxMove, x, y, dp):
+        if x < 0 or x >= self.m or y < 0 or y >= self.n:
+            return 1
+        if maxMove <= 0:
+            return 0
+        if dp[x][y][maxMove] is not None:
+            return dp[x][y][maxMove]
+        res = 0
 
-            for i in range(m):
-                for j in range(n):
-                    if i == m - 1:
-                        count = (count + dp[i][j]) % M
-                    if j == n - 1:
-                        count = (count + dp[i][j]) % M
-                    if i == 0:
-                        count = (count + dp[i][j]) % M
-                    if j == 0:
-                        count = (count + dp[i][j]) % M
-                    temp[i][j] = (
-                        ((dp[i - 1][j] if i > 0 else 0) + (dp[i + 1][j] if i < m - 1 else 0)) % M +
-                        ((dp[i][j - 1] if j > 0 else 0) + (dp[i][j + 1] if j < n - 1 else 0)) % M
-                    ) % M
+        res = (res + self.helper(maxMove - 1, x + 1, y, dp)) % self.mod
+        res = (res + self.helper(maxMove - 1, x, y - 1, dp)) % self.mod
+        res = (res + self.helper(maxMove - 1, x - 1, y, dp)) % self.mod
+        res = (res + self.helper(maxMove - 1, x, y + 1, dp)) % self.mod
 
-            dp = temp
+        dp[x][y][maxMove] = res
 
-        return count
-
+        return res

e599577e427fb5d90c9a781bd1be65b6f0c5d94c
Rushikesh Sanjay Deshmukh <138613542+Rushi264@users.noreply.github.com>
2024-02-09 09:37:15 -0800

    Time: 205 ms (88.35%) | Memory: 16.7 MB (96.03%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.py
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -1,25 +1,26 @@
 class Solution:
-    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        nums.sort()
+    def largestDivisibleSubset(self, nums):
         n = len(nums)
+        if n == 0:
+            return []
+        
+        nums.sort()
         dp = [1] * n
-        max_size, max_index = 1, 0
-
+        prev_index = [-1] * n
+        maxi = 0
+        
         for i in range(1, n):
             for j in range(i):
-                if nums[i] % nums[j] == 0:
-                    dp[i] = max(dp[i], dp[j] + 1)
-                    if dp[i] > max_size:
-                        max_size = dp[i]
-                        max_index = i
-
+                if nums[i] % nums[j] == 0 and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+                    prev_index[i] = j
+                    if dp[i] > dp[maxi]:
+                        maxi = i
+        
         result = []
-        num = nums[max_index]
-        for i in range(max_index, -1, -1):
-            if num % nums[i] == 0 and dp[i] == max_size:
-                result.append(nums[i])
-                num = nums[i]
-                max_size -= 1
-
+        while maxi >= 0:
+            result.append(nums[maxi])
+            maxi = prev_index[maxi]
+        
+        result.reverse()
         return result
-

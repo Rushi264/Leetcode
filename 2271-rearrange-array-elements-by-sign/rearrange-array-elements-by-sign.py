e09125d152d2714fed854870741dcfb83644ebbc
Rushikesh Sanjay Deshmukh <138613542+Rushi264@users.noreply.github.com>
2024-02-13 18:22:22 -0800

    Time: 1053 ms (32.13%) | Memory: 44.9 MB (99.06%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -2,16 +2,17 @@ class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         array1 = []
         array2 = []
-        final = []
-        i,j = 0,0
+        index = 0
         for i in nums:
             if i > 0:
                 array1.append(i)
             else:
                 array2.append(i)
         for x in range(len(array1)):
-            final.append(array1[x])
-            final.append(array2[x])
+            nums[index] = array1[x]
+            index += 1
+            nums[index] = array2[x]
+            index += 1
 
-        return final
+        return nums
 

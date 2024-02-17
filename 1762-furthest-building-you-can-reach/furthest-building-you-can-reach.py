062b15cff7d803f9d26785bb70ba228989cc697d
Rushikesh Sanjay Deshmukh <138613542+Rushi264@users.noreply.github.com>
2024-02-17 14:54:37 -0800

    Time: 445 ms (27.72%) | Memory: 31.1 MB (76.54%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.py
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.py
@@ -1,27 +1,13 @@
 class Solution:
-    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
-        if len(heights) == 1:
-            return 0
-        pq = []
-        i = 0
-        while i < len(heights) - 1:
-            if heights[i + 1] <= heights[i]:
-                i += 1
-                continue
-            diff = heights[i + 1] - heights[i]
-            if bricks >= diff:
-                bricks -= diff
-                heapq.heappush(pq, -diff)
-            elif ladders > 0:
-                if pq:
-                    past_bricks = -pq[0]
-                    if past_bricks > diff:
-                        bricks += past_bricks
-                        heapq.heappop(pq)
-                        bricks -= diff
-                        heapq.heappush(pq, -diff)
-                ladders -= 1
-            else:
-                break
-            i += 1
-        return i
+    def furthestBuilding(
+        self, heights: List[int], bricks: int, ladders: int
+    ) -> int:
+        jumps = []
+        for i, (h0, h1) in enumerate(pairwise(heights)):
+            if (d := h1 - h0) > 0:
+                heapq.heappush(jumps, d)
+            if len(jumps) > ladders:
+                bricks -= heapq.heappop(jumps)
+            if bricks < 0:
+                return i
+        return len(heights) - 1

6cc118604ae241ce6836cf08086931557c562ea3
Rushikesh Sanjay Deshmukh <138613542+Rushi264@users.noreply.github.com>
2024-02-17 15:01:27 -0800

    Time: 412 ms (87.87%) | Memory: 31 MB (76.54%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.py
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.py
@@ -1,13 +1,19 @@
 class Solution:
-    def furthestBuilding(
-        self, heights: List[int], bricks: int, ladders: int
-    ) -> int:
-        jumps = []
-        for i, (h0, h1) in enumerate(pairwise(heights)):
-            if (d := h1 - h0) > 0:
-                heapq.heappush(jumps, d)
-            if len(jumps) > ladders:
-                bricks -= heapq.heappop(jumps)
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        heap = []
+
+        for i in range(len(heights) - 1):
+            diff = heights[i+1] - heights[i]
+            if diff <= 0:
+                continue
+            
+            bricks -= diff
+            heapq.heappush(heap, -diff)
+
             if bricks < 0:
-                return i
-        return len(heights) - 1
+                if ladders == 0:
+                    return i
+                ladders -= 1
+                bricks += -heapq.heappop(heap)
+
+        return len(heights) - 1 

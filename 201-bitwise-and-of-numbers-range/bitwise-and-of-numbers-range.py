c9b57162c3db3ade1ba68ee8d07771e382013afa
Rushikesh Sanjay Deshmukh <138613542+Rushi264@users.noreply.github.com>
2024-02-21 15:13:24 -0800

    Time: 50 ms (69.33%) | Memory: 16.5 MB (80.67%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
@@ -1,10 +1,8 @@
-class Solution(object):
-    def rangeBitwiseAnd(self, left, right):
-        shift = 0
-        # Find the common prefix bits
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        cnt = 0
         while left != right:
             left >>= 1
             right >>= 1
-            shift += 1
-        # Append zeros to the right
-        return left << shift
+            cnt += 1
+        return left << cnt

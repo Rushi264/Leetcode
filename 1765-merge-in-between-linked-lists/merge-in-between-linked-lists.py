89cf74f37f9a13e2f361693a9a171438369eeeed
Rushikesh Sanjay Deshmukh <138613542+Rushi264@users.noreply.github.com>
2024-03-19 22:09:43 -0400

    Time: 192 ms (58.89%) | Memory: 21 MB (70.92%) - LeetSync

--- 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.py
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.py
@@ -1,38 +1,14 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def mergeInBetween(self, list1, a, b, list2):
-        # Create a dummy node to handle cases where the removal starts from the head of list1
-        dummy = ListNode(0)
-        dummy.next = list1
-        
-        # Traverse to the (a-1)th node
-        current = dummy
-        for i in range(a):
-            current = current.next
-        
-        # Save the reference to the (a-1)th node
-        before_a = current
-        
-        # Traverse to the bth node
-        for i in range(a, b + 1):
-            current = current.next
-        
-        # Save the reference to the (b+1)th node
-        after_b = current.next
-        
-        # Connect the (a-1)th node to list2
-        before_a.next = list2
-        
-        # Traverse to the end of list2
-        while list2.next:
-            list2 = list2.next
-        
-        # Connect the end of list2 to the (b+1)th node
-        list2.next = after_b
-        
-        return dummy.next
-
+    def mergeInBetween(
+        self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
+        pt = hd = list1
+        for _ in range(a - 1):
+            pt = pt.next
+        pta = pt.next
+        pt.next = list2
+        while pt.next:
+            pt = pt.next
+        for _ in range(b - a):
+            pta = pta.next
+        pt.next = pta.next
+        return hd

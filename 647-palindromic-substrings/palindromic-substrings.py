a68ef5061ff7d0fdf51469fd1a03bfcd2010535f
Rushikesh Sanjay Deshmukh <138613542+Rushi264@users.noreply.github.com>
2025-11-11 14:59:06 -0800

    Time: 134 ms (46.21%) | Memory: 17.7 MB (86.95%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,20 +1,24 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        res = 0
+        count = 0
+
+        # helper to expand around center
+        def expand(l, r):
+            nonlocal count
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
 
         for i in range(len(s)):
-            l = r = i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                res += 1
-                l -= 1
-                r += 1
-            
-            l = i 
-            r = i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                res += 1
-                l -= 1
-                r += 1
-        return res
+            # odd-length palindromes (center at i)
+            expand(i, i)
+            # even-length palindromes (center between i and i+1)
+            expand(i, i + 1)
 
-        
+        return count
+    
+    #this is quite simple version of Palindromic Substrings we used same logic but this time weed need total number of
+    # palidrome exist in the string. so there are two cases 1st is for odd length palindrome and 2nd is for even 
+    # and we created a helper function to write our main lgoic which constists of if its palindrome or not and if it
+    # is then just increment our count.

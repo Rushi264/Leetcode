8577dd3a3be586c880bc22ba63afd59011875943
Rushikesh Sanjay Deshmukh <138613542+Rushi264@users.noreply.github.com>
2025-11-12 12:54:31 -0800

    Time: 447 ms (79.47%) | Memory: 43.7 MB (27.01%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,21 +1,19 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # Get the lengths of both input strings
-        len_text1, len_text2 = len(text1), len(text2)
-      
-        # Initialize a 2D array (list of lists) with zeros for dynamic programming
-        # The array has (len_text1 + 1) rows and (len_text2 + 1) columns
-        dp_matrix = [[0] * (len_text2 + 1) for _ in range(len_text1 + 1)]
-      
-        # Loop through each character index of text1 and text2
-        for i in range(1, len_text1 + 1):
-            for j in range(1, len_text2 + 1):
-                # If the characters match, take the diagonal value and add 1
-                if text1[i - 1] == text2[j - 1]:
-                    dp_matrix[i][j] = dp_matrix[i - 1][j - 1] + 1
+        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+        # first for loop is for column and 2 for loop is for rows.
+
+        for i in range(len(text1) - 1, -1, -1):
+        #we are iterating thorugh rows in reverse order since it bottom up approach
+            for j in range(len(text2) - 1, -1, -1):
+            #we are iterating thorugh columns in reverse order since it bottom up approach
+                if text1[i] == text2[j]:
+                    #if 2 values matches then we just add 1 in the bottom diagonal grid and
+                    #store it in our current value
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
                 else:
-                    # If the characters do not match, take the maximum of the value from the left and above
-                    dp_matrix[i][j] = max(dp_matrix[i - 1][j], dp_matrix[i][j - 1])
-      
-        # The bottom-right value in the matrix contains the length of the longest common subsequence
-        return dp_matrix[len_text1][len_text2]
+                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+                     #if 2 values doesn't matches then we will take the maximum of 
+                     # right grid and the bottom grid and store it in current grid.
+        return dp[0][0]
+        

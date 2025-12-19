f6d5af6a20697134866c5baba7b95f494dafd50e
Rushikesh Sanjay Deshmukh <138613542+Rushi264@users.noreply.github.com>
2025-12-19 12:01:23 -0800

    Time: 17 ms (40.48%) | Memory: 21.7 MB (37.07%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,17 +1,9 @@
-from collections import defaultdict 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = defaultdict(list)
-        
-        # Iterate through each string in the input list
+        res = defaultdict(list)
         for s in strs:
-            # Sort the characters in the string and use it as a key
-            sorted_s = ''.join(sorted(s))
-            
-            # Append the original string to the list of anagrams with the same key
-            anagrams[sorted_s].append(s)
-        
-        # Convert the values of the dictionary to a list to get the grouped anagrams
-        grouped_anagrams = list(anagrams.values())
-        
-        return grouped_anagrams
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())

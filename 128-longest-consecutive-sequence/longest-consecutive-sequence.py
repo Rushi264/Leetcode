b8ccf9108bda6df8080b753f4294be0b0db2258b
Rushikesh Sanjay Deshmukh <138613542+Rushi264@users.noreply.github.com>
2025-12-22 18:18:31 -0800

    Time: 42 ms (89.65%) | Memory: 34.2 MB (53.92%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -3,6 +3,8 @@ class Solution:
         numSet = set(nums)
         maxLen = 0
         for n in numSet:
+            # we using numSet and not nums beacuse there might be duplicate values in nums and dont want to
+            #evaluate those duplicate so to aviod these we can iterate thorough set and actual nums.
             if (n - 1) not in numSet:
                 #check if the number is start of the series or not skip it and find the start of the series
                 #of numbers.

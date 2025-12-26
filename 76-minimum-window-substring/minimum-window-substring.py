fa9d25da7e094b5ae16d2de359347d1962c0866f
Rushikesh Sanjay Deshmukh <138613542+Rushi264@users.noreply.github.com>
2025-12-26 12:44:16 -0800

    Time: 61 ms (82.16%) | Memory: 18 MB (91.27%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,40 +1,44 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not s or not t:
+        if t == "": 
             return ""
 
-        dictT = defaultdict(int)
+        countT , window = {}, {}
+
         for c in t:
-            dictT[c] += 1
+            countT[c] = 1 + countT.get(c, 0)
+        
+        have, need = 0, len(countT)
 
-        required = len(dictT)
-        l, r = 0, 0
-        formed = 0
+        res, resLen = [-1,-1], float("infinity")
+        l = 0
 
-        windowCounts = defaultdict(int)
-        ans = [-1, 0, 0]
-
-        while r < len(s):
+        for r in range(len(s)):
             c = s[r]
-            windowCounts[c] += 1
+            window[c] = 1 + window.get(c, 0)
 
-            if c in dictT and windowCounts[c] == dictT[c]:
-                formed += 1
-
-            while l <= r and formed == required:
-                c = s[l]
-
-                if ans[0] == -1 or r - l + 1 < ans[0]:
-                    ans[0] = r - l + 1
-                    ans[1] = l
-                    ans[2] = r
-
-                windowCounts[c] -= 1
-                if c in dictT and windowCounts[c] < dictT[c]:
-                    formed -= 1
-
+            if c in countT and window[c] == countT[c]: 
+                have += 1
+        
+            while have == need:
+                if (r - l + 1) < resLen:
+                    res = [l,r]
+                    resLen = r - l + 1
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
                 l += 1
 
-            r += 1
+        l , r = res
 
-        return "" if ans[0] == -1 else s[ans[1]:ans[2] + 1]
+        return s[l:r + 1] if resLen != float("infinity") else ""
+
+            
+
+
+
+
+
+        
+
+        

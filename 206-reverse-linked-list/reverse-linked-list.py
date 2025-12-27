7a6ef5c1034773188792915658fa8d12ff1dc3dd
Rushikesh Sanjay Deshmukh <138613542+Rushi264@users.noreply.github.com>
2025-12-27 12:05:05 -0800

    Time: 0 ms (100.00%) | Memory: 18 MB (94.31%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -6,10 +6,18 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev , curr = None, head
-
+        # we are doin this iteratively. 
         while curr:
             temp = curr.next
+            #store our curr.next in temperary variable, before assigning it to prev.
             curr.next = prev
+        #None -->  1 --> 2 --> 3 --> 4 --> 5
+        #prev   curr   curr.next
+
+        #None <-- 1 -/- 2 --> 3 --> 4 --> 5
+        #None   prev   temp  curr.next
+
+        # this process will continue till whole list is inverted.
             prev = curr
             curr = temp
         return prev 

cfcd7d40643590de43de82c3d93f4adcdbb28e6d
Rushikesh Sanjay Deshmukh <138613542+Rushi264@users.noreply.github.com>
2025-12-29 14:11:59 -0800

    Time: 0 ms (100.00%) | Memory: 17.3 MB (88.27%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,19 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-
-        # Base cases
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-
-        # Initialize an array for dynamic programming
-        dp = [0] * (n + 1)
-        dp[1], dp[2] = 1, 2
-
-        # Fill the array with the number of ways to climb to each step
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-
-        return dp[n]
         
+        one_step, two_step = 1, 1
+    #we are treating it like a fibonacci series, here we are going with bottom-up approach in DP.
+    #just watch the neetcode explaination for more details.
+        for i in range(n - 1):
+            temp = one_step # to store our value one in order to shift it(to store it in two)
+            one_step = one_step + two_step
+            two_step = temp
+        
+        return one_step
